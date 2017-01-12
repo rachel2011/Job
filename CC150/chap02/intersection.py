a66fb2e3f5075c43af8cd0c84f5cbb2f7cafdc01
REN ZHOU <renzhou@rens-MacBook-Pro.local>
2017-01-12 07:01:34 -0600

    no message

---
+++ CC150/chap02/intersection.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+        curA, curB = headA,headB
+        lenA = lenB = 0
+        while curA:
+            lenA += 1
+            curA = curA.next
+        while curB:
+            lenB += 1
+            curB = curB.next
+        curA, curB = headA,headB
+        if lenA > lenB:
+            for i in range(lenA-lenB):
+                curA = curA.next
+        elif lenB > lenA:
+            for i in range(lenB-lenA):
+                curB = curB.next
+        while curA != curB:
+            curA = curA.next
+            curB = curB.next
+        return curA
